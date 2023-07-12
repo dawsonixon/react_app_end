eb8db15a944127ea5f0636e7e3c04d6f1d86cf53
dawsonixon <135368982+dawsonixon@users.noreply.github.com>
2023-07-12 20:11:35 +0200

    Add files via upload

---
+++ react_app/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'React App',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Integracja aplikacji React',
+    'sequence': 1,
+    'author': 'Twój Nick',
+    'website': 'https://twojastrona.com',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/homepage.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
